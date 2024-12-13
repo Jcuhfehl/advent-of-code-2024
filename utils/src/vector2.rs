118e185d220269a68c419bd8f3313c82ebeb708b
Freya Winters <freya@gosan.nl>
2024-12-13 09:19:42 +0100

    Add grid to utils

--- utils/src/vector2.rs
+++ utils/src/vector2.rs
@@ -73,3 +73,71 @@ impl std::ops::Sub for Vector2i {
         self + -rhs
     }
 }
+
+#[derive(Debug)]
+struct Grid<T> {
+    default: T,
+    size: usize,
+    values: Vec<Vec<T>>,
+}
+
+impl<T> Grid<T> {
+    fn get(&self, location: &Vector2i) -> &T {
+        if location.x < 0
+            || location.x >= self.size as i64
+            || location.y < 0
+            || location.y >= self.size as i64
+        {
+            return &self.default;
+        }
+        &self.values[location.x as usize][location.y as usize]
+    }
+
+    fn get_mut(&mut self, location: &Vector2i) -> &mut T {
+        if location.x < 0
+            || location.x >= self.size as i64
+            || location.y < 0
+            || location.y >= self.size as i64
+        {
+            return &mut self.default;
+        }
+        &mut self.values[location.x as usize][location.y as usize]
+    }
+
+    fn set(&mut self, location: &Vector2i, value: T) -> bool {
+        if location.x < 0
+            || location.x >= self.size as i64
+            || location.y < 0
+            || location.y >= self.size as i64
+        {
+            return false;
+        }
+        self.values[location.x as usize][location.y as usize] = value;
+        true
+    }
+
+    fn coordinates(&self) -> Vec<Vector2i> {
+        (0..self.size)
+            .flat_map(|y| {
+                (0..self.size).map(move |x| Vector2i {
+                    x: x as i64,
+                    y: y as i64,
+                })
+            })
+            .collect()
+    }
+}
+
+impl<T: Clone> Grid<T> {
+    fn empty(size: usize, default: T) -> Grid<T> {
+        let values = (0..size)
+            .map(|_| (0..size).map(|_| default.clone()).collect())
+            .collect();
+
+        Grid::<T> {
+            default,
+            size,
+            values,
+        }
+    }
+}
