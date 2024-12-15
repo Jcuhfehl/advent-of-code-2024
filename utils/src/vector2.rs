e9f1579be6ac9a1994393a41d6562010350728b8
Freya Winters <freya@gosan.nl>
2024-12-15 13:50:55 +0100

    Improve utilities

--- utils/src/vector2.rs
+++ utils/src/vector2.rs
@@ -25,6 +25,19 @@ impl Vector2i {
     pub fn new(x: i64, y: i64) -> Vector2i {
         Vector2i { x, y }
     }
+
+    pub fn modulo(self: Vector2i, rhs: Vector2i) -> Vector2i {
+        let mut x = self.x % rhs.x;
+        let mut y = self.y % rhs.y;
+        if x < 0 {
+            x = rhs.x + x;
+        }
+        if y < 0 {
+            y = rhs.y + y;
+        }
+
+        Vector2i { x, y }
+    }
 }
 
 impl std::ops::Add for Vector2i {
@@ -75,40 +88,40 @@ impl std::ops::Sub for Vector2i {
 }
 
 #[derive(Debug)]
-struct Grid<T> {
-    default: T,
-    size: usize,
-    values: Vec<Vec<T>>,
+pub struct Grid<T> {
+    pub default: T,
+    pub size: Vector2i,
+    pub values: Vec<Vec<T>>,
 }
 
 impl<T> Grid<T> {
-    fn get(&self, location: &Vector2i) -> &T {
+    pub fn get(&self, location: &Vector2i) -> &T {
         if location.x < 0
-            || location.x >= self.size as i64
+            || location.x >= self.size.x
             || location.y < 0
-            || location.y >= self.size as i64
+            || location.y >= self.size.y
         {
             return &self.default;
         }
         &self.values[location.x as usize][location.y as usize]
     }
 
-    fn get_mut(&mut self, location: &Vector2i) -> &mut T {
+    pub fn get_mut(&mut self, location: &Vector2i) -> &mut T {
         if location.x < 0
-            || location.x >= self.size as i64
+            || location.x >= self.size.x
             || location.y < 0
-            || location.y >= self.size as i64
+            || location.y >= self.size.y
         {
             return &mut self.default;
         }
         &mut self.values[location.x as usize][location.y as usize]
     }
 
-    fn set(&mut self, location: &Vector2i, value: T) -> bool {
+    pub fn set(&mut self, location: &Vector2i, value: T) -> bool {
         if location.x < 0
-            || location.x >= self.size as i64
+            || location.x >= self.size.x
             || location.y < 0
-            || location.y >= self.size as i64
+            || location.y >= self.size.y
         {
             return false;
         }
@@ -116,10 +129,10 @@ impl<T> Grid<T> {
         true
     }
 
-    fn coordinates(&self) -> Vec<Vector2i> {
-        (0..self.size)
+    pub fn coordinates(&self) -> Vec<Vector2i> {
+        (0..self.size.y)
             .flat_map(|y| {
-                (0..self.size).map(move |x| Vector2i {
+                (0..self.size.x).map(move |x| Vector2i {
                     x: x as i64,
                     y: y as i64,
                 })
@@ -129,9 +142,9 @@ impl<T> Grid<T> {
 }
 
 impl<T: Clone> Grid<T> {
-    fn empty(size: usize, default: T) -> Grid<T> {
-        let values = (0..size)
-            .map(|_| (0..size).map(|_| default.clone()).collect())
+    pub fn empty(size: Vector2i, default: T) -> Grid<T> {
+        let values = (0..size.x)
+            .map(|_| (0..size.y).map(|_| default.clone()).collect())
             .collect();
 
         Grid::<T> {
