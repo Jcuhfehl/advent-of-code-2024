0fbd0807c13ef45f3bc1d3714f91632c22340797
Freya Winters <freya@gosan.nl>
2024-12-17 21:29:43 +0100

    Improve utils

--- utils/src/main.rs
+++ utils/src/main.rs
@@ -5,4 +5,12 @@ fn main() {
     let input_with_trailing =
         std::fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let input = input_with_trailing.trim_end();
+
+    let _ = parse_input(TEST_INPUT);
 }
+
+fn parse_input(input: &str) {
+    todo!()
+}
+
+const TEST_INPUT: &str = "";
